0777b0ba31238c0a1763b144aab984d5bb4b924b
khlee2 <khlee2@eduwill.net>
2022-03-16 14:27:31 +0900

    first commit

---
+++ RPA_V1/src/main/java/com/edw/rpa/RpaV1Application.java
@@ -0,0 +1,13 @@
+package com.edw.rpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpaV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RpaV1Application.class, args);
+    }
+
+}
---
+++ RPA_V1/src/test/java/com/edw/rpa/RpaV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edw.rpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpaV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
